eaeaaf3a511b32909aacacc9d0e76b4fae44a714
Synthwave84 <wjddnd877@gmail.com>
2023-08-01 17:42:35 +0900

    2023-0801-003

--- React_study/src/index.js
+++ React_study/src/index.js
@@ -1,24 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import Book from './chapter_03/Book'
-import Library from './chapter_03/Library';
-import Clock from './chapter_04/Clock';
-import TestApp from './chapter_04/TestApp';
-import reportWebVitals from './reportWebVitals';
-import ComponentList from './chapter_05/ComponentList';
-import Employee from './chapter_05/Employee';
-import EmplolyeeList from './chapter_05/EmployeeList';
+// import App from './App';
+// import Book from './chapter_03/Book'
+// import Library from './chapter_03/Library';
+// import Clock from './chapter_04/Clock';
+// import TestApp from './chapter_04/TestApp';
+
+// import ComponentList from './chapter_05/ComponentList';
+// import Employee from './chapter_05/Employee';
+// import EmplolyeeList from './chapter_05/EmployeeList';
 // import Hello from './chapter_06/Hello';
 // import Hello from './chapter_07/Hello';
-import Hello from './chapter_08/Hello.jsx'
+// import Hello from './chapter_08/Hello.jsx'
+// import Hello from './chapter_09/Hello.jsx'
+import Hello from './chapter_10/Hello.jsx'
+import Wrapper from './chapter_09/Wrapper'
+
+import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render (
   <React.StrictMode>
+    <Wrapper>
     <Hello name="홍길동" age={100} color="red" />
+    <Hello color="pink" />
+    </Wrapper>
 </React.StrictMode>
 )
 
