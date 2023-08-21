a0449814e7aec2fb53c19748c5e4b25a9087dc0c
Synthwave84 <wjddnd877@gmail.com>
2023-08-21 14:38:36 +0900

    2023-0821-002

--- next-app/src/app/read/[id]/page.js
+++ next-app/src/app/read/[id]/page.js
@@ -1,11 +1,15 @@
-export default function Read(props) {
+export default async function Read(props) {
   
   console.log('props', props);
 
+  const id = props.params.id;
+  const resp = await fetch(`http://localhost:9999/topics/${id}`); // 백엔드 스프링 주소.
+  const topic = await resp.json();
+  
   return (
     <>
-      <h2>Read</h2>
-      parameter: {props.params.id}
+      <h2>{topic.title}</h2>
+      {topic.body}
     </>
   );
 }
