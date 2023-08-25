e1a967d2043b5a258c27c8f8a520155022eb80a6
Synthwave84 <wjddnd877@gmail.com>
2023-08-25 17:43:12 +0900

    2023-0825-001

---
+++ Eclipse_Workspace/springjava/src/ch02/sec11/VariableScopeExample.java
@@ -0,0 +1,15 @@
+package ch02.sec11;
+
+public class VariableScopeExample {
+	public static void main(String[] args) {
+//		변수 사용 범위
+//		블록 {} 안에 선언된 변수는 블록 밖에선 사용할 수 없다.
+		
+		int v1=15;
+		if(v1>10) {
+			int v2 = v1- 10; // 지역변수
+		}
+//		int v3= v1 + v2 + 5;  v2 변수는 사용할 수가 없다. 
+		
+	}
+}
---
+++ Eclipse_Workspace/springjava/src/ch03/sec01/SignOperatorExample.java
@@ -0,0 +1,19 @@
+package ch03.sec01;
+
+public class SignOperatorExample {
+
+	public static void main(String[] args) {
+
+		int x = -100;
+		x = -x; 
+		System.out.println(x);
+		
+		byte b= 100;
+		int y = -b;
+		System.out.println(y);
+		
+		
+		
+		
+}
+}
