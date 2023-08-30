6f482677acde7717b5f6b4b6cadc186983f19251
Synthwave84 <wjddnd877@gmail.com>
2023-08-30 20:55:29 +0900

    abcd

--- todo-app-master/src/App.js
+++ todo-app-master/src/App.js
@@ -3,9 +3,29 @@ import './style.css';
 
 function App() {
   return (
-    <div className="App">
-    
+    <div className="todoapp">
+    <h1>Todo List</h1>
+    <input id="new-todo-title"  className="new-todo"></input>
+    <main>
+      <input className="toggle-all" type="checkbox"/>
+      <ul id="todo-list" className="todo=list"></ul>
+      <div className="count-container">
+        <span className="todo-count">총 <strong>0</strong>개</span>
+        <ul className="filters">
+          <li>
+            <a className="all selected" href="#">해야 할 일</a>
+          </li>
+          <li>
+            <a className="active" href="#">전체 보기</a>
+          </li>
+          <li>
+            <a className="completed" href="#">완료한 일</a>
+          </li>
+        </ul>
+      </div>
+    </main>
     </div>
+
   );
 }
 
