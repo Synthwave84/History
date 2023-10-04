8364ab17e674dd28f6dea7d687204538adf76161
Synthwave84 <wjddnd877@gmail.com>
2023-10-04 10:11:46 +0900

    2023-1004-000

---
+++ Eclipse_Workspace/springjava/src/ch19/sec04/NewsServer.java
@@ -0,0 +1,16 @@
+package ch19.sec04;
+
+import java.net.DatagramSocket;
+
+//	UDP 통신 네트워크 예제
+public class NewsServer {
+	
+	private static DatagramSocket datagramSocket = null;
+		
+	
+	public static void main(String[] args) {
+		System.out.println("--------------------------------------------");
+		System.out.println("서버 종료시 Q입력 후 Enter 를 입력하세요");
+		System.out.println("--------------------------------------------");
+	}
+}
